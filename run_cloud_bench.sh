676b0c2df7b997c838e4c5390d8bb909cf2358ba
jedeland10 <johan.edeland@live.com>
2025-03-20 19:58:25 +0100

    tune cloud script

--- run_cloud_bench.sh
+++ run_cloud_bench.sh
@@ -1,6 +1,6 @@
 # Ensure we have exactly four parameters
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 <output_file_path> <run_index> <keyprefixsize> <url_prefix>"
+if [ "$#" -ne 6 ]; then
+  echo "Usage: $0 <output_file_path> <run_index> <keyprefixsize> <url_prefix> <record_count> <operation_count>"
   exit 1
 fi
 
@@ -9,6 +9,8 @@ output_file_path="$1"   # e.g. "../ycsb_bench/cache/once"
 run_index="$2"          # e.g. "1"
 keyprefixsize="$3"      # e.g. "4096"
 url_prefix="$4"         # e.g. "http://127.0.0.1:12380/"
+record_count="$5"
+operation_count="$6"
 
 # Build the output file name: output_file_path/{keyprefixsize}_{run_index}.txt
 output_file="${output_file_path}/${keyprefixsize}_${run_index}.txt"
@@ -20,7 +22,7 @@ echo "TEST Performing load phase..." | tee -a "$output_file"
 
 ./bin/ycsb load rest -P workloads/workload_write \
     -p url.prefix="$url_prefix" \
-    -p recordcount=10000 -p operationcount=1 \
+    -p recordcount="$record_count" -p operationcount=1 \
     -p keyprefixsize="$keyprefixsize" \
     -p insertproportion=1 -p updateproportion=0 -p fieldcount=1 \
     | grep -E '^\[OVERALL\]|^\[INSERT\]' | tee -a "$output_file"
@@ -30,5 +32,5 @@ echo "---------------------------------------" | tee -a "$output_file"
 ./bin/ycsb run rest -P workloads/workload_write \
     -p url.prefix="$url_prefix" \
     -p keyprefixsize="$keyprefixsize" \
-    -p recordcount=10000 -p operationcount=500000 -p fieldcount=1 2>&1 \
+    -p recordcount="$record_count" -p operationcount="$operation_count" -p fieldcount=1 2>&1 \
     | grep -E '^\[OVERALL\]|^\[UPDATE\]' | tee -a "$output_file"
